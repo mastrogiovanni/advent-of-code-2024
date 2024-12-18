dee9ebcc37f4c616bd96d770000f7b2f6607de5e
Michele Mastrogiovanni <michele.mastrogiovanni@elerian.ai>
2024-12-18 10:13:08 +0100

    other exercises

--- cmd/es16/main.go
+++ cmd/es16/main.go
@@ -17,13 +17,21 @@ type TraversableGraph struct {
 	Nodes map[int]*Node
 }
 
-type NodePath struct {
-	Node *Node
-	Next *NodePath
+type VisitContext struct {
+	Target  utility.Point
+	Visited map[int]bool
+	Path    []*Node
+	Cost    int
+	Best    *Solution
 }
 
-func NodeId(px, py int, w, h int) int {
-	return py*w + px
+type Solution struct {
+	Path []*Node
+	Cost int
+}
+
+func (tg *TraversableGraph) NodeId(px, py int) int {
+	return py*tg.Graph.Width + px
 }
 
 func NewTraversableGraph(graph *utility.CharGraph) *TraversableGraph {
@@ -40,7 +48,7 @@ func NewTraversableGraph(graph *utility.CharGraph) *TraversableGraph {
 }
 
 func CreateNodes(tg *TraversableGraph, px, py int) *Node {
-	nodeId := NodeId(px, py, tg.Graph.Width, tg.Graph.Height)
+	nodeId := tg.NodeId(px, py)
 	test, ok := tg.Nodes[nodeId]
 	if ok {
 		return test
@@ -60,7 +68,7 @@ func CreateNodes(tg *TraversableGraph, px, py int) *Node {
 		if tg.Graph.Get(nx, ny) == '#' {
 			continue
 		}
-		neighborId := NodeId(nx, ny, tg.Graph.Width, tg.Graph.Height)
+		neighborId := tg.NodeId(nx, ny)
 		node, ok := tg.Nodes[neighborId]
 		if !ok {
 			node = CreateNodes(tg, nx, ny)
@@ -71,43 +79,200 @@ func CreateNodes(tg *TraversableGraph, px, py int) *Node {
 	return resultNode
 }
 
-func Visit(tg *TraversableGraph, position, end utility.Point, visited map[int]bool, path []*Node) []*Node {
-	node := tg.Nodes[NodeId(position.X, position.Y, tg.Graph.Width, tg.Graph.Height)]
-	visited[node.Id] = true
-	if position.X == end.X && position.Y == end.Y {
-		// End
-		return append(path, node)
+func IsTurn(path []*Node, index int) bool {
+	if index+1 >= len(path) {
+		return false
 	}
-	var minDist []*Node = nil
-	for _, neighbor := range node.Neighbors {
-		if _, ok := visited[neighbor.Id]; ok {
-			continue
+	if index-1 < 0 {
+		return false
+	}
+	dx1 := path[index].Position.X - path[index-1].Position.X
+	dx2 := path[index+1].Position.X - path[index].Position.X
+	if dx1 != dx2 {
+		return true
+	}
+	dy1 := path[index].Position.Y - path[index-1].Position.Y
+	dy2 := path[index+1].Position.Y - path[index].Position.Y
+	return dy1 != dy2
+}
+
+// var bestSolution *Solution = nil
+
+// func Dump(context *VisitContext) {
+// 	context.Cost -= 1
+// 	dx := context.Path[1].Position.X - context.Path[0].Position.X
+// 	if dx == 0 {
+// 		// vertical
+// 		context.Cost += 1000
+// 	} else {
+// 		if dx < 0 {
+// 			context.Cost += 2000
+// 		}
+// 	}
+// 	if bestSolution == nil {
+// 		bestSolution = &Solution{
+// 			Path: context.Path,
+// 			Cost: context.Cost,
+// 		}
+// 	} else {
+// 		if bestSolution.Cost > context.Cost {
+// 			bestSolution = &Solution{
+// 				Path: context.Path,
+// 				Cost: context.Cost,
+// 			}
+// 		}
+// 	}
+// }
+
+func Next(neighbors []*Node, past *Node) *Node {
+	if neighbors[0] == past {
+		return neighbors[1]
+	} else {
+		return neighbors[0]
+	}
+}
+
+// Return the path from position to end and its cost
+func Visit(tg *TraversableGraph, position utility.Point, context *VisitContext) *Solution {
+
+	// nodeId := tg.NodeId(position.X, position.Y)
+	// node := tg.Nodes[nodeId]
+	// queue := list.New()
+	// queue.PushBack(node)
+
+	// parents := make(map[int]*Node)
+	// parents[node.Id] = nil
+
+	// for queue.Len() > 0 {
+	// 	currentNode := queue.Front().Value.(*Node)
+	// 	queue.Remove(queue.Front())
+
+	// 	// compare if node is equals to target
+	// 	if currentNode.Position.X == context.Target.X && currentNode.Position.Y == context.Target.Y {
+	// 		// the target has been looked
+	// 		// reconstructing the path
+	// 		var route []*Node
+	// 		for len(currentNode.Value) > 0 {
+	// 			// recreating route
+	// 			route = append([]string{currentNode}, route...)
+	// 			// changing pointer
+	// 			currentNode.Value = parents[currentNode.Value]
+	// 		}
+
+	// 		// returning path result
+	// 		return strings.Join(route, "->")
+	// 	}
+
+	// 	for _, neighbor := range node.Neighbors {
+	// 		// check if the neighbor has not already been visited
+	// 		if _, visited := context.Visited[neighbor.Id]; !visited {
+	// 			// add neighbor to parents map associated to current node value
+	// 			parents[neighbor.Id] = currentNode
+	// 			// add neighbor to the end of the queue
+	// 			queue.PushBack(neighbor)
+	// 		}
+	// 	}
+	// }
+
+	// log.Println(len(context.Path))
+	nodeId := tg.NodeId(position.X, position.Y)
+	node := tg.Nodes[nodeId]
+	path := append(context.Path, node)
+	context.Visited[nodeId] = true
+	toUnvisit := make([]int, 0)
+	toUnvisit = append(toUnvisit, nodeId)
+
+	defer func() {
+		for _, nodeId := range toUnvisit {
+			delete(context.Visited, nodeId)
 		}
-		d := Visit(tg, neighbor.Position, end, visited, append(path, node))
-		if d == nil {
-			continue
-		}
-		if minDist == nil {
-			minDist = d
+	}()
+
+	// +1000 if this node was reached after a turn
+	turnCost := 1
+	if IsTurn(path, len(path)-2) {
+		turnCost = 1000 + 1
+	}
+
+	// If Visited the target node
+	if position.X == context.Target.X && position.Y == context.Target.Y {
+		// Dump(&VisitContext{
+		// 	Path: path,
+		// 	Cost: context.Cost + turnCost,
+		// })
+		dx := context.Path[1].Position.X - context.Path[0].Position.X
+		cost := context.Cost + turnCost - 1
+		if dx == 0 {
+			// vertical
+			cost += 1000
 		} else {
-			if len(minDist) < len(d) {
-				minDist = d
+			if dx < 0 {
+				cost += 2000
 			}
 		}
+		solution := &Solution{
+			Path: path,
+			Cost: cost,
+		}
+		if context.Best == nil {
+			context.Best = solution
+		} else {
+			if solution.Cost < context.Best.Cost {
+				context.Best = solution
+			}
+		}
+		return context.Best
 	}
-	visited[node.Id] = false
-	return minDist
+
+	for _, neighbor := range node.Neighbors {
+
+		if _, ok := context.Visited[neighbor.Id]; ok {
+			continue
+		}
+
+		if context.Best != nil && context.Best.Cost < context.Cost+turnCost {
+			continue
+		}
+
+		solution := Visit(tg, neighbor.Position, &VisitContext{
+			Target:  context.Target,
+			Visited: context.Visited,
+			Path:    path,
+			Cost:    context.Cost + turnCost,
+		})
+		if solution == nil {
+			continue
+		}
+
+		if context.Best == nil {
+			context.Best = solution
+		} else {
+			if solution.Cost < context.Best.Cost {
+				context.Best = solution
+			}
+		}
+
+	}
+
+	return context.Best
+
 }
 
 func main() {
-	graph := utility.NewGraph("cmd/es16/test.txt")
+	graph := utility.NewGraph("cmd/es16/input.txt")
 	tg := NewTraversableGraph(graph)
-	log.Println(tg)
 	start := graph.Find('S')
 	end := graph.Find('E')
-	visited := make(map[int]bool)
-	distance := Visit(tg, start, end, visited, make([]*Node, 0))
-	for _, node := range distance {
-		log.Println(node)
+	context := &VisitContext{
+		Target:  end,
+		Visited: make(map[int]bool),
 	}
+	log.Println("Starting")
+	solution := Visit(tg, start, context)
+	log.Println("Solution")
+	for _, item := range solution.Path {
+		log.Println(item)
+	}
+	log.Println(solution.Cost)
+
 }
--- cmd/es17/main.go
+++ cmd/es17/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"log"
-	"reflect"
+	"slices"
 	"strings"
 
 	"github.com/mastrogiovanni/advent-of-code-2024/src/utility"
 )
 
 type Registers struct {
-	A uint64
-	B uint64
-	C uint64
+	A int
+	B int
+	C int
 }
 
-func (r *Registers) GetComboOperand(index uint64) uint64 {
+func (r *Registers) GetComboOperand(index int) int {
 	if index < 4 {
 		return index
 	}
@@ -27,54 +27,44 @@ func (r *Registers) GetComboOperand(index uint64) uint64 {
 	if index == 6 {
 		return r.C
 	}
-	log.Fatalf("Error")
 	return 0
 }
 
-func Processor(program []uint64, registers *Registers, limit int) []uint64 {
-	output := make([]uint64, 0)
-	var pc uint64 = 0
-	for int(pc) < len(program) {
-
+func Processor(program []int, registers *Registers) []int {
+	t := registers.A
+	output := make([]int, 0)
+	for pc := 0; pc < len(program); {
 		instruction := program[pc]
-		// log.Println("Instruction", instruction)
 		operand := program[pc+1]
+		combo := registers.GetComboOperand(operand)
 		pc = pc + 2
-
 		switch instruction {
 		case 0: // adv: OK
-			registers.A = (registers.A >> registers.GetComboOperand(operand))
+			registers.A >>= combo
 		case 1: // bxl
-			registers.B = (registers.B ^ operand)
+			registers.B ^= operand
 		case 2: // bst
-			registers.B = (registers.GetComboOperand(operand) % 8)
+			registers.B = combo % 8
 		case 3: // jnz
 			if registers.A != 0 {
 				pc = operand
+				continue
 			}
 		case 4: // bxc
-			registers.B = (registers.B ^ registers.C)
+			registers.B ^= registers.C
 		case 5: // out
-			output = append(output, registers.GetComboOperand(operand)%8)
-			if limit > 0 && len(output) > limit {
-				return []uint64{}
-			}
+			output = append(output, combo%8)
 		case 6: // bdv
-			registers.B = (registers.A >> registers.GetComboOperand(operand))
+			registers.B = (registers.A >> combo)
 		case 7: // cdv
-			registers.C = (registers.A >> registers.GetComboOperand(operand))
+			registers.C = (registers.A >> combo)
 		}
 	}
+	log.Println(t, output)
 	return output
 }
 
-// Register A: 729
-// Register B: 0
-// Register C: 0
-
-// Program: 0,1,5,4,3,0
-
-func Evaluate(a, b, c uint64, program []uint64) {
+func Evaluate(a, b, c int, program []int) {
 	log.Println()
 	log.Println("Evaluate")
 	registers := Registers{
@@ -82,55 +72,61 @@ func Evaluate(a, b, c uint64, program []uint64) {
 		B: b,
 		C: c,
 	}
-	output := Processor(program, &registers, -1)
-	outputStringList := utility.UInt64ToStrList(output)
+	output := Processor(program, &registers)
+	outputStringList := utility.IntToStrList(output)
 	outputString := strings.Join(outputStringList, ",")
 	log.Println("Output:", outputString)
 	log.Println("Registers:", registers)
 }
 
-func FindCopy(a, b, c uint64, program []uint64) {
-	i := uint64(2.8e+14)
-	for ; ; i++ {
+func Part2(b, c int, program []int) {
+	a := 0
+	for i := len(program) - 1; i >= 0; i-- {
+		// make room for 3 bits by shifting to the left
+		a <<= 3
+		// check incrementally only the latest bits,
+		// until we find the right value
 		registers := Registers{
-			A: uint64(i),
+			A: a,
 			B: b,
 			C: c,
 		}
-		output := Processor(program, &registers, len(program))
-		// log.Println(uint64(i), len(program), len(output))
-		if len(output) != len(program) {
-			// log.Println("Skip")
-			continue
-		}
-		if reflect.DeepEqual(output, program) {
-			// log.Println("Found", i)
-			break
+		for !slices.Equal(Processor(program, &registers), program[i:]) {
+			a++
+			registers = Registers{
+				A: a,
+				B: b,
+				C: c,
+			}
 		}
 	}
+	log.Println(a)
 }
 
 func main() {
-	// Evaluate(0, 0, 9, []uint64{2, 6})
-	// Evaluate(10, 0, 0, []uint64{5, 0, 5, 1, 5, 4})
-	// Evaluate(2024, 0, 0, []uint64{0, 1, 5, 4, 3, 0})
-	// Evaluate(0, 29, 0, []uint64{1, 7})
-	// Evaluate(0, 2024, 43690, []uint64{4, 0})
-	// Evaluate(729, 0, 0, []uint64{0, 1, 5, 4, 3, 0})
+	// Evaluate(0, 0, 9, []int{2, 6})
+	// Evaluate(10, 0, 0, []int{5, 0, 5, 1, 5, 4})
+	// Evaluate(2024, 0, 0, []int{0, 1, 5, 4, 3, 0})
+	// Evaluate(0, 29, 0, []int{1, 7})
+	// Evaluate(0, 2024, 43690, []int{4, 0})
+	// Evaluate(729, 0, 0, []int{0, 1, 5, 4, 3, 0})
 
-	// // part 1
-	// // 2,4,1,1,7,5,4,0,0,3,1,6,5,5,3,0
-	// Evaluate(30899381, 0, 0, []uint64{2, 4, 1, 1, 7, 5, 4, 0, 0, 3, 1, 6, 5, 5, 3, 0})
+	// Part 1
+	// Evaluate(30899381, 0, 0, []int{2, 4, 1, 1, 7, 5, 4, 0, 0, 3, 1, 6, 5, 5, 3, 0})
 
 	// part2
-	// FindCopy(0, 0, 0, []uint64{0, 3, 5, 4, 3, 0})
-	FindCopy(30899381, 0, 0, []uint64{2, 4, 1, 1, 7, 5, 4, 0, 0, 3, 1, 6, 5, 5, 3, 0})
+	// FindCopy(0, 0, 0, []int{0, 3, 5, 4, 3, 0})
+	Part2(0, 0, []int{2, 4, 1, 1, 7, 5, 4, 0, 0, 3, 1, 6, 5, 5, 3, 0})
+
+	// 4: a
+	// 5: b
+	// 6: c
 
 	// b = a 				// 2, 4
 	// b = b ^ 1			// 1, 1
 	// c = a >> b 			// 7, 5
 	// b = b ^ c 			// 4, 0,
-	// a = a >> 3  		// 0, 3,
+	// a = a >> 3  			// 0, 3,
 	// b = b ^ 6  			// 1, 6,
 	// output(b%8)			// 5, 5,
 	// if a != 0 return 0 	// 3, 0
